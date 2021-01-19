85dff4565a8e55d95eea3716feb66262e3e16e38
Cesar <cmartinezaraujo@eagles.ewu.edu>
2021-01-18 22:41:22 -0800

    Added file path member and fixed new line issue when appending

---
+++ Logger/FileLogger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Logger
+{
+    class FileLogger : BaseLogger
+    {
+        private String? filePath;
+
+        public FileLogger(String newFilePath)
+        {
+            if (newFilePath is null) throw new ArgumentNullException(nameof(FileLogger));
+
+            filePath = newFilePath;
+        }
+
+        public override void Log(LogLevel logLevel, string message)
+        {
+            //Check date X
+            //Check if on new line X
+            File.AppendAllText(filePath,$"{DateTime.Now} {ClassName} {logLevel}: {message}");
+            File.AppendAllText(filePath, Environment.NewLine);
+        }
+    }
+}
