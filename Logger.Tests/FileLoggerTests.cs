c73fe35552cd3b2be2ea3ce2c08b71dcd3d42d73
Cesar <cmartinezaraujo@eagles.ewu.edu>
2021-01-18 22:48:10 -0800

    Added test to check for successful logging

--- Logger.Tests/FileLoggerTests.cs
+++ Logger.Tests/FileLoggerTests.cs
@@ -1,10 +1,36 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
 
 namespace Logger.Tests
 {
     [TestClass]
     public class FileLoggerTests
     {
-        
+
+
+        [TestMethod]
+        public void FileLogger_Makes_Successful_Logs()
+        {
+            //Arrange
+            string testPath = Path.GetRandomFileName();
+
+            LogFactory logFactory = new LogFactory();
+            logFactory.ConfigureFileLogger(testPath);
+
+            var logger = logFactory.CreateLogger(nameof(FileLoggerTests));
+
+            //Act
+            logger.Log(LogLevel.Debug, "Testing");
+            logger.Log(LogLevel.Warning, "Testing");
+            logger.Debug("Msg: {0}", 34);
+
+            //Assert
+            string[] lines = File.ReadAllLines(testPath);
+            File.Delete(testPath);
+            Assert.AreEqual(3, lines.Length);
+        }
+
+
     }
 }
