1f6ad0d54b0d9f2a3dc534c5f9c0cb08c33de321
Cesar <cmartinezaraujo@eagles.ewu.edu>
2021-01-18 22:48:37 -0800

    Added tests to check returned Logger

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -6,6 +6,43 @@ namespace Logger.Tests
     [TestClass]
     public class LogFactoryTests
     {
-        
+
+        [TestMethod]
+        public void Logger_Returns_Null()
+        {
+            LogFactory logFactory = new LogFactory();
+
+            BaseLogger testLogger = logFactory.CreateLogger(nameof(testLogger));
+
+            Assert.IsNull(testLogger);
+        }
+
+        [TestMethod]
+        public void Logger_Returns_Configured_Logger()
+        {
+            string testPath = Path.GetRandomFileName();
+
+            LogFactory logFactory = new LogFactory();
+            logFactory.ConfigureFileLogger(testPath);
+
+            BaseLogger testLogger = logFactory.CreateLogger(nameof(testLogger));
+
+            Assert.IsNotNull(testLogger);
+
+        }
+
+       [TestMethod]
+       public void Create_Logger_ClassName_Set()
+        {
+            string testPath = Path.GetRandomFileName();
+
+            LogFactory logFactory = new LogFactory();
+            logFactory.ConfigureFileLogger(testPath);
+
+            BaseLogger testLogger = logFactory.CreateLogger(nameof(testLogger));
+
+            Assert.AreEqual(testLogger.ClassName, nameof(testLogger));
+        }
+
     }
 }
