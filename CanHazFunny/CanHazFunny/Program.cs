8145116c91fde47b6e398ec61caab4524d4c2262
Cesar <cmartinezaraujo@eagles.ewu.edu>
2021-01-25 23:11:29 -0800

    Made an instance of Jester to get a joke and display it

--- CanHazFunny/CanHazFunny/Program.cs
+++ CanHazFunny/CanHazFunny/Program.cs
@@ -4,8 +4,10 @@
     {
         static void Main(string[] args)
         {
-            //Feel free to use your own setup here - this is just provided as an example
-            //new Jester(new SomeReallyCoolOutputClass(), new SomeJokeServiceClass()).TellJoke();
+
+            Jester joker = new(new JokeService(), new JokeDisplay());
+            joker.TellJoke();
+
         }
     }
 }
