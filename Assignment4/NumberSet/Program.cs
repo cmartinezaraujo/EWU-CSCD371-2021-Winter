138b5770c876dbb32ee47c867d70c238897dda37
Cesar <cmartinezaraujo@eagles.ewu.edu>
2021-02-01 22:10:32 -0800

    Just used for extra testing to console

---
+++ Assignment4/NumberSet/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NumberSet
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           // NumSet nums = new NumSet(1, 2, 3, 4, 3, 2, 1, 5);
+            //int[] array = nums.returnArray();
+            
+            //for(int x = 0; x < array.Length; x++)
+            //{
+             //   Console.WriteLine(array[x]);
+            //}
+        }
+    }
+}
