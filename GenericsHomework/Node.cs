34349dd650d4b4371e7485d088c2c39cbfe985ab
Cesar <cmartinezaraujo@eagles.ewu.edu>
2021-02-08 17:33:42 -0800

    Fixed typo

--- GenericsHomework/Node.cs
+++ GenericsHomework/Node.cs
@@ -40,5 +40,15 @@ namespace GenericsHomework
             Node<T> newNode = new Node<T>(value);
             this.Next = newNode;
         }
+
+        public void Clear()
+        {
+            Next = this;
+            /*This should be enough to clear a list from the curent
+             Node. There is no need to close the loop the GC should be able to
+            detect the other nodes as unreachable references and eventually
+            collect them. Keeping the loop open also does not effect the list 
+            when then inserting or deleting nodes later on*/
+        }
     }
 }
