a7a56966921173260fa8a7b82bfabb1c87c83f26
Cesar <cmartinezaraujo@eagles.ewu.edu>
2021-02-22 21:27:30 -0800

    Implemented methods

--- Assignment/Assignment/SampleData.cs
+++ Assignment/Assignment/SampleData.cs
@@ -1,30 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Assignment
 {
     public class SampleData : ISampleData
     {
         // 1.
-        public IEnumerable<string> CsvRows => throw new NotImplementedException();
-
+        public IEnumerable<string> CsvRows => File.ReadLines("People.csv").Skip(1);
+     
+ 
         // 2.
         public IEnumerable<string> GetUniqueSortedListOfStatesGivenCsvRows() 
-            => throw new NotImplementedException();
+            => CsvRows.Select(record => record = record.Split(",")[6]).OrderBy(state => state).Distinct().ToList();
 
         // 3.
         public string GetAggregateSortedListOfStatesUsingCsvRows()
-            => throw new NotImplementedException();
+            => string.Join(",", GetUniqueSortedListOfStatesGivenCsvRows().ToArray());
 
         // 4.
-        public IEnumerable<IPerson> People => throw new NotImplementedException();
+        public IEnumerable<IPerson> People => CsvRows.OrderBy(record => record = record.Split(",")[6]).
+            ThenBy(record => record = record.Split(",")[5]).ThenBy(record => record = record.Split(",")[7]).
+            Select(record => { string[] info = record.Split(","); return new Person(info[1], info[2], new Address(info[4], info[5], info[6], info[7]), info[3]); });
 
         // 5.
         public IEnumerable<(string FirstName, string LastName)> FilterByEmailAddress(
-            Predicate<string> filter) => throw new NotImplementedException();
+            Predicate<string> filter) => People.Where( person => filter.Invoke(person.EmailAddress)).Select(person => (person.FirstName,person.LastName));
 
         // 6.
         public string GetAggregateListOfStatesGivenPeopleCollection(
-            IEnumerable<IPerson> people) => throw new NotImplementedException();
+            IEnumerable<IPerson> people) => people.Select(person => person.Address.State).Distinct()
+            .Aggregate((result, next) => result + "," + next);
     }
 }
