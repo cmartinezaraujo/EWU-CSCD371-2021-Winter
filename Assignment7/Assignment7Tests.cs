2b09864ffde2c6b4f34808d55893709b0e1d1680
Cesar <cmartinezaraujo@eagles.ewu.edu>
2021-03-06 23:47:37 -0800

    Fixed some tests

--- Assignment7/Assignment7Tests.cs
+++ Assignment7/Assignment7Tests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using System.Net;
 using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Assignment7
@@ -12,12 +13,14 @@ namespace Assignment7
         string[] urls = { "https://google.com", "https://google.com", "https://google.com", "https://google.com", "https://google.com", "https://google.com" };
         WebClient client = new WebClient();
 
-        public int repetitionsCall(int repetitions)
+        public int RepetitionsCallHelper(int repetitions)
         {
             CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
-            CancellationToken cancellationToken = cancellationTokenSource.Token;
+            ParallelOptions parallelOptions = new ParallelOptions();
+            parallelOptions.CancellationToken = cancellationTokenSource.Token;
+
             var progress = new Progress<int>(progressPercent => Console.WriteLine(progressPercent));
-            int result = (Assignment7.DownloadTextRepeatedlyAsynch(progress, cancellationToken, repetitions, urls)).Result;
+            int result = (Assignment7.DownloadTextRepeatedlyAsynch(progress, parallelOptions, repetitions, urls)).Result;
             return result;
         }
 
@@ -38,7 +41,7 @@ namespace Assignment7
         [TestMethod]
         public void DownloadTextRepeatedlyAsynchCountsCharacters_ReturnsMoreThan0()
         {
-            Assert.IsTrue(repetitionsCall(1) > 0);
+            Assert.IsTrue(RepetitionsCallHelper(1) > 0);
         }
 
         [TestMethod]
@@ -47,33 +50,46 @@ namespace Assignment7
             int characterExpected = urls.Aggregate(0, (total, next) => total + client.DownloadString(next).Length);
             characterExpected += urls.Aggregate(0, (total, next) => total + client.DownloadString(next).Length);
 
-            Assert.IsTrue((repetitionsCall(2) - characterExpected) < 10000);
+            Assert.IsTrue((RepetitionsCallHelper(2) - characterExpected) < 10000);
         }
 
         [TestMethod]
+        [ExpectedException(typeof(AggregateException))]
         public void DownloadTextRepeatedlyAsynch_Returns0WhenCanceledBeforCall()
         {
             CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
-            CancellationToken cancellationToken = cancellationTokenSource.Token;
+            ParallelOptions parallelOptions = new ParallelOptions();
+            parallelOptions.CancellationToken = cancellationTokenSource.Token;
             cancellationTokenSource.Cancel();
+
             var progress = new Progress<int>(progressPercent => Console.WriteLine(progressPercent));
 
 
-            int result = (Assignment7.DownloadTextRepeatedlyAsynch(progress, cancellationToken, 100, urls)).Result;
+            int result = (Assignment7.DownloadTextRepeatedlyAsynch(progress, parallelOptions, 100, urls)).Result;
 
             Assert.AreEqual<int>(0, result);
         }
 
         [TestMethod]
+        [ExpectedException(typeof(AggregateException))]
         public void DownloadTextRepeatedlyAsynch_GetsCancelledAfter10Milliseconds()
         {
-            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
-            CancellationToken cancellationToken = cancellationTokenSource.Token;
-            cancellationTokenSource.CancelAfter(10);
+            CancellationTokenSource cancellationTokenSource1 = new CancellationTokenSource();
+            CancellationTokenSource cancellationTokenSource2 = new CancellationTokenSource();
+
+            ParallelOptions parallelOptions1 = new ParallelOptions();
+            parallelOptions1.CancellationToken = cancellationTokenSource1.Token;
+
+            ParallelOptions parallelOptions2 = new ParallelOptions();
+            parallelOptions2.CancellationToken = cancellationTokenSource2.Token;
+
+
+            cancellationTokenSource2.CancelAfter(10);
+
             var progress = new Progress<int>(progressPercent => Console.WriteLine(progressPercent));
 
-            int expected = (Assignment7.DownloadTextRepeatedlyAsynch(progress, cancellationToken, 5, urls)).Result;
-            int result = (Assignment7.DownloadTextRepeatedlyAsynch(progress, cancellationToken, 5, urls)).Result;
+            int expected = (Assignment7.DownloadTextRepeatedlyAsynch(progress, parallelOptions1, 5, urls)).Result;
+            int result = (Assignment7.DownloadTextRepeatedlyAsynch(progress, parallelOptions2, 5, urls)).Result;
 
             Assert.IsTrue(expected > result);
 
@@ -85,11 +101,13 @@ namespace Assignment7
             int progressTriggered = 0;
 
             CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
-            CancellationToken cancellationToken = cancellationTokenSource.Token;
+            ParallelOptions parallelOptions = new ParallelOptions();
+            parallelOptions.CancellationToken = cancellationTokenSource.Token;
+
             var progress = new Progress<int>(progressPercent => progressTriggered++);
 
             
-            int expected = (Assignment7.DownloadTextRepeatedlyAsynch(progress, cancellationToken, 3, urls)).Result;
+            int expected = (Assignment7.DownloadTextRepeatedlyAsynch(progress, parallelOptions, 3, urls)).Result;
 
             Assert.AreEqual<int>(3, progressTriggered);
         }
