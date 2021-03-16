bc67abf4d75c166212d16f7fb5e9ab2f9c443006
Cesar <cmartinezaraujo@eagles.ewu.edu>
2021-03-16 12:53:28 -0700

    Added view model for a contact

---
+++ ContactApp/ContactViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ContactApp
+{
+    public class ContactViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string TwitterHandle { get; set; }
+
+        //ADD DATE TIME LAST MODIFIED
+
+
+    }
+
+}
