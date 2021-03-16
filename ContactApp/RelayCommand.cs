cda82fdb8d57e3a938dc29e42ed7f5df06d4593b
Cesar <cmartinezaraujo@eagles.ewu.edu>
2021-03-16 12:56:22 -0700

    Added to handle some commands

---
+++ ContactApp/RelayCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ContactApp
+{
+    public class RelayCommand : ICommand
+    {
+        private Action ExecuteDelegate { get; }
+        private Func<bool> CanExecuteDelegate { get; }
+
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => CanExecuteDelegate.Invoke();
+        public void Execute(object parameter) => ExecuteDelegate.Invoke();
+        public RelayCommand(Action executeDelegate, Func<bool> canExecuteDelegate)
+        {
+            ExecuteDelegate = executeDelegate ?? throw new ArgumentNullException(nameof(executeDelegate));
+            CanExecuteDelegate = canExecuteDelegate ?? throw new ArgumentNullException(nameof(canExecuteDelegate));
+        }
+    }
+
+}
