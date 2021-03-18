853f2691f79389ab8f993514686debda6703e058
Cesar <cmartinezaraujo@eagles.ewu.edu>
2021-03-18 15:02:06 -0700

    Added more tests

--- ContactApp.Tests/MainWindowViewModelTests.cs
+++ ContactApp.Tests/MainWindowViewModelTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ContactApp.Tests
@@ -8,15 +10,76 @@ namespace ContactApp.Tests
     {
         [TestMethod]
 
-        public void OnNewContact_AddsNewContactToList()
+        public void NewContactCommand_AddsNewContactToList()
         {
-            var vm = new MainWindowViewModel();
+            var viewModel = new MainWindowViewModel();
 
-            int before = vm.Contacts.Count;
+            int before = viewModel.Contacts.Count;
 
-            vm.NewContactCommand.Execute(null);
+            viewModel.NewContactCommand.Execute(null);
 
-            Assert.AreEqual<int>(before + 1, vm.Contacts.Count);
+            Assert.AreEqual<int>(before + 1, viewModel.Contacts.Count);
+        }
+
+        [TestMethod]
+        public void DeleteContactCommand_RemovesSelectedContactFromList()
+        {
+            var viewModel = new MainWindowViewModel();
+
+            int before = viewModel.Contacts.Count;
+
+            var contact = viewModel.Contacts.Last();
+
+            viewModel.SelectedContact = contact;
+
+            viewModel.DeleteContactCommand.Execute(null);
+
+            Assert.AreEqual<int>(before - 1, viewModel.Contacts.Count);
+            Assert.IsFalse(viewModel.Contacts.Contains(contact));
+        }
+
+        [TestMethod]
+
+        public void EditContactCommand_SetsIsBeingEditedToTrue()
+        {
+            var vieModel = new MainWindowViewModel();
+
+            vieModel.IsBeingEdited = false;
+
+            vieModel.EditContactCommand.Execute(null);
+
+            Assert.IsTrue(vieModel.IsBeingEdited);
+        }
+
+        [TestMethod]
+        public void SaveContactCommand_SetsIsBeingEditedToFalse()
+        {
+            var viewModel = new MainWindowViewModel();
+
+            viewModel.IsBeingEdited = true;
+
+            viewModel.SelectedContact = viewModel.Contacts.First();
+
+            viewModel.SaveContactCommand.Execute(null);
+
+            Assert.IsFalse(viewModel.IsBeingEdited);
+        }
+
+        [TestMethod]
+        public void SaveContactCommand_UpdatesLastModifiedDate()
+        {
+            var viewModel = new MainWindowViewModel();
+
+            viewModel.SelectedContact = viewModel.Contacts.First();
+
+            DateTime originalDate = DateTime.Now;
+
+            viewModel.SelectedContact = viewModel.Contacts.First();
+            viewModel.SelectedContact.LastModified = originalDate;
+
+            viewModel.SaveContactCommand.Execute(null);
+
+            Assert.AreNotEqual<DateTime>(originalDate, viewModel.SelectedContact.LastModified);
         }
 
     }
