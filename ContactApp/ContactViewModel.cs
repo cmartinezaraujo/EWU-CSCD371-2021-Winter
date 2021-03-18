ef927acf22f64b162a4a0d429bc15fb54258f33e
Cesar <cmartinezaraujo@eagles.ewu.edu>
2021-03-17 17:11:24 -0700

    Changed the visibility of the contact info to change with style triggers instead of the converter

--- ContactApp/ContactViewModel.cs
+++ ContactApp/ContactViewModel.cs
@@ -1,28 +1,73 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace ContactApp
 {
     public class ContactViewModel : INotifyPropertyChanged
     {
 
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string PhoneNumber { get; set; }
-        public string EmailAddress { get; set; }
-        public string TwitterHandle { get; set; }
+        private string? _FirstName;
+        public string? FirstName 
+        { 
+            get => _FirstName;
+            set => SetProperty(ref _FirstName!, value!);
+        }
+
+        private string? _LastName;
+        public string? LastName 
+        {
+            get => _LastName; 
+            set => SetProperty(ref _LastName!, value!);
+        }
+
+        private string? _PhoneNumber;
+        public string? PhoneNumber 
+        {
+            get => _PhoneNumber; 
+            set => SetProperty(ref _PhoneNumber!, value!);
+        }
+
+        private string? _EmailAddress;
+        public string? EmailAddress 
+        {
+            get => _EmailAddress; 
+            set => SetProperty(ref _EmailAddress!, value!);
+        }
+
+        private string? _TwitterHandle;
+        public string? TwitterHandle 
+        {
+            get => _TwitterHandle; 
+            set => SetProperty(ref _TwitterHandle!, value!);
+        }
 
         private DateTime _LastModified;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public DateTime LastModified 
-        { 
+        public DateTime LastModified
+        {
             get => _LastModified;
             set
             {
                 _LastModified = value;
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LastModified)));
+            }
+        }
+
+        private void SetProperty(ref string field, string newValue, [CallerMemberName] string propertyName = "")
+        {
+            if(String.Equals("", newValue.Trim()))
+            {
+                newValue = "";
+            }
+
+            if (!String.Equals(field, newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
             }
         }
 
